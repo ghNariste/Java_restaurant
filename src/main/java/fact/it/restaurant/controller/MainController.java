01be0b0449d794ed054f9b7f5d712ea8bde17dfa
Nariste <r0926901@student.thomasmore.be>
2023-08-25 11:22:06 +0200

    html pages

--- src/main/java/fact/it/restaurant/controller/MainController.java
+++ src/main/java/fact/it/restaurant/controller/MainController.java
@@ -1,2 +1,22 @@
-package fact.it.restaurant.controller;public class MainController {
+package fact.it.restaurant.controller;
+
+import fact.it.restaurant.model.Reservation;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+@Controller
+public class MainController {
+    private ArrayList<String> restaurantList = new ArrayList<>();
+    private ArrayList<Reservation> reservationList = new ArrayList<>();
+    @RequestMapping("/reservation")
+    public String reservation
+
+
 }
--- src/main/java/fact/it/restaurant/model/Reservation.java
+++ src/main/java/fact/it/restaurant/model/Reservation.java
@@ -1,2 +1,57 @@
-package fact.it.restaurant.model;public class Reservation {
+package fact.it.restaurant.model;
+
+import java.time.LocalDate;
+
+
+public class Reservation {
+    private String name;
+    private LocalDate date;
+    private int numberOfPeople;
+    private boolean vegetarian;
+    private String restaurantName;
+
+    public Reservation() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public void setNumberOfPeople(int numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public void setVegetarian(boolean vegetarian) {
+        this.vegetarian = vegetarian;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+
 }
